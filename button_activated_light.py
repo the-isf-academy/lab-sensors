f38868aab6b3d72b5963d2635ccfcfa9b95dca16
emmaqbrown <eqbrown@isf.edu.hk>
2021-08-30 10:23:48 +0800

    add commit template

--- button_activated_light.py
+++ button_activated_light.py
@@ -1,7 +1,6 @@
 #####################################
-#button_activated_light.py
+# button_activated_light.py
 # Teachers: Emma Brown and Britte Genzlinger
 # Student: YOUR NAME GOES HERE
 ####################################
 from gpiozero import LED
-from gpiozero import Button
