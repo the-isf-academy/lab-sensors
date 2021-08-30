76126e6084836c8a4e65604bf75bbb905185800d
brittegenzlinger <31494508+brittegenzlinger@users.noreply.github.com>
2021-08-30 10:02:35 +0800

    Create button_activated_light.py
    Add header and import statements

---
+++ button_activated_light.py
@@ -0,0 +1,7 @@
+#####################################
+#button_activated_light.py
+# Teachers: Emma Brown and Britte Genzlinger
+# Student: YOUR NAME GOES HERE
+####################################
+from gpiozero import LED
+from gpiozero import Button
